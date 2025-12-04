4ce36c39c9874a3ebfbc3a30d06f7bc117467aa3
Tom Marcheron <tom.marcheron@etu.unicaen.fr>
2025-12-04 10:13:47 +0100

    fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
-  outDir: 'public',
-  publicDir: 'static',
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+	outDir: 'public',
+  	publicDir: 'static',
 });
