7bad7f39ab6de40a7485f79a98f1979faca8a9ee
Tom Marcheron <tom.marcheron@etu.unicaen.fr>
2025-12-09 15:23:06 +0100

    Modification de la page index.php

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Portfolio de Tom Marcheron';
+export const SITE_DESCRIPTION = 'Bienvenue, je suis Tom Marcheron, étudiant en deuxième année de BUT MMI. J\'ai rassemblé ici mes projets universitaires et personnels ainsi que mes compétences techniques.';
