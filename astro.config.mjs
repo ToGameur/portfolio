7f06ee6126aaee88d4b64f888115119063b6b6e1
Tom Marcheron <tom.marcheron@etu.unicaen.fr>
2026-01-06 16:20:09 +0100

    feat: update site URL and remove `outDir` and `publicDir` configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://portfolio-b96939.pages.unicaen.fr',
 	base: '/portfolio',
 	integrations: [mdx(), sitemap()],
-	outDir: 'public',
-  	publicDir: 'static',
 });
