2d0c55e7331d91c172793d5c7aef92554a87ffe4
Tom Marcheron <tom.marcheron@etu.unicaen.fr>
2026-01-06 16:53:48 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
-// @ts-check
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	site: 'https://portfolio-b96939.pages.unicaen.fr',
-	base: '/portfolio',
-	integrations: [mdx(), sitemap()],
-	outDir: 'public',
-  	publicDir: 'static',
+	site: "https://portfolio-b96939.pages.unicaen.fr",
+	base: "/portfolio",
 });
